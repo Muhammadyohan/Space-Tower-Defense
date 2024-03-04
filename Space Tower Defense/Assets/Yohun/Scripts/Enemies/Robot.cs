4f52a7159bd0ebfe81e67a1701a88bae6f74a248
Muhammadyohan <142879314+Muhammadyohan@users.noreply.github.com>
2024-03-04 15:39:04 +0700

    add Robot Enemy Ai

--- Space Tower Defense/Assets/Yohun/Scripts/Enemies/Robot.cs
+++ Space Tower Defense/Assets/Yohun/Scripts/Enemies/Robot.cs
@@ -3,12 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class GreenAlien : MonoBehaviour
+public class Robot : MonoBehaviour
 {
     private Enemy thisEnemy;
     public NavMeshAgent agent;
-    public Transform player;
-    public LayerMask whatIsGround, whatIsPlayer, whatIsDefendedObject;
+    public LayerMask whatIsGround, whatIsDefendedObject;
 
     [Header("Go to Target Object")]
     public Transform targetPoint;
@@ -21,12 +20,11 @@ public class GreenAlien : MonoBehaviour
     public EnemyAttack enemyAttack;
 
     [Header("States")]
-    public float sightRange, attackRange;
-    public bool playerInSightRange, playerInAttackRange, targetInAttackRange;
+    public float attackRange;
+    public bool targetInAttackRange;
 
     void Awake()
     {
-        StartCoroutine(GetPlayerObj());
         thisEnemy = GetComponent<Enemy>();
         thisEnemy.Init();
         targetPoint = GameObject.Find("Shield Core").transform;
@@ -36,29 +34,12 @@ public class GreenAlien : MonoBehaviour
     void Update()
     {
         // Check for sight and attack range
-        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
-        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
         targetInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsDefendedObject);
 
         if (!isAttacking)
         {
-            if (playerInSightRange && !playerInAttackRange) ChasePlayer();
-            else if (playerInAttackRange && playerInSightRange) AttackPlayer();
-            else if (targetInAttackRange) AttackTargetObject();
-            else if (!playerInSightRange && !playerInAttackRange) GoToTargetObject();
-        }
-    }
-
-    private IEnumerator GetPlayerObj()
-    {
-        yield return new WaitForSeconds(0.1f);
-        try
-        {
-            player = GameObject.Find("PlayerObj").transform;
-        }
-        catch
-        {
-            StartCoroutine(GetPlayerObj());
+            if (targetInAttackRange) AttackTargetObject();
+            else GoToTargetObject();
         }
     }
 
@@ -72,38 +53,6 @@ public class GreenAlien : MonoBehaviour
                 thisEnemy.animator.SetBool("Move", true);
             }
         }
-    }
-
-    private void ChasePlayer()
-    {
-        agent.SetDestination(player.position);
-        if(!thisEnemy.animator.GetBool("Move"))
-        {
-            thisEnemy.animator.SetBool("Move", true);
-        }
-    }
-
-    private void AttackPlayer()
-    {
-        // Make sure enemy doesn't move
-        agent.SetDestination(transform.position);
-        if(thisEnemy.animator.GetBool("Move"))
-            thisEnemy.animator.SetBool("Move", false);
-
-        if (shouldRotate)
-        {
-            // Rotate enemy to look at the player
-            transform.LookAt(new Vector3(player.position.x, 0, player.position.z));
-        } 
-
-        if (!alreadyAttacked)
-        {
-            // Attack code here
-            thisEnemy.animator.SetTrigger("Attack");
-            isAttacking = true;
-            alreadyAttacked = true;
-            Invoke(nameof(ResetAttack), timeBetweenAttacks);
-        } 
     }
 
     private void AttackTargetObject()
